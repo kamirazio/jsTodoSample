49a3850b6c6181923255959d7d54950367292299
kamirazio <kamirazio@gmail.com>
2017-12-10 18:07:07 +1000

    Merge branch 'dev_front'

--- myscript.js
+++ myscript.js
@@ -3,14 +3,27 @@
 // Front DV
 // おおまかな機能をつくる
   // イベントの作成
-  // --- inputに入力した言葉を取得する
+  function newTask(){
 
-  // 要素の追加
+    // 言葉を取得し新要素を生成する
+    // --- inputに入力した言葉を取得する
+    var task_input = document.getElementById("task_input")
+    var inputValue = task_input.value;
+    console.log(inputValue);
+    var str = document.createTextNode(inputValue);
+    // 要素の追加
+    // Create a new list item when clicking on the "Add" button
+    var li = document.createElement("li");
+    li.appendChild(str);
+    // 全体に足す
+    document.getElementById("task_list").appendChild(li);
+    task_input.value = "";
+    
+  }
+
   //  --- 取得した言葉をタスク要素にする
   //  --- タスクリストに要素を追加する
   //  --- 入力したらinputを空にする
 
   // 条件分岐
   // --- inputが空っぽなら注意する
-
-console.log("Hello JS !");
