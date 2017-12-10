97e0eee0c19ecd6d9ba3802d29c24d9e70b709f4
kamirazio <kamirazio@gmail.com>
2017-12-10 17:49:34 +1000

    リスト要素の追加成功
    Front_DV

--- myscript.js
+++ myscript.js
@@ -6,11 +6,19 @@
   // --- inputに入力した言葉を取得する
 
   // 要素の追加
+  // Create a new list item when clicking on the "Add" button
+  console.log("Hello JS TODO list !");
+
+  // 言葉を取得し新要素を生成する
+  var str = document.createTextNode("new task");
+  var li = document.createElement("li");
+  li.appendChild(str);
+  // 全体に足す
+  document.getElementById("task_list").appendChild(li);
+
   //  --- 取得した言葉をタスク要素にする
   //  --- タスクリストに要素を追加する
   //  --- 入力したらinputを空にする
 
   // 条件分岐
   // --- inputが空っぽなら注意する
-
-console.log("Hello JS !");
