778b4058fec1d529c21d665ef6f28f63f659ff2b
kamirazio <kamirazio@gmail.com>
2017-12-10 17:53:57 +1000

    Addボタンのイベントを追加
    Masterをマージ済み

--- myscript.js
+++ myscript.js
@@ -3,18 +3,21 @@
 // Front DV
 // おおまかな機能をつくる
   // イベントの作成
-  // --- inputに入力した言葉を取得する
+  function newTask(){
+    console.log("Hello JS TODO list !");
 
-  // 要素の追加
-  // Create a new list item when clicking on the "Add" button
-  console.log("Hello JS TODO list !");
+    // --- inputに入力した言葉を取得する
 
-  // 言葉を取得し新要素を生成する
-  var str = document.createTextNode("new task");
-  var li = document.createElement("li");
-  li.appendChild(str);
-  // 全体に足す
-  document.getElementById("task_list").appendChild(li);
+    // 要素の追加
+    // Create a new list item when clicking on the "Add" button
+
+    // 言葉を取得し新要素を生成する
+    var str = document.createTextNode("new task");
+    var li = document.createElement("li");
+    li.appendChild(str);
+    // 全体に足す
+    document.getElementById("task_list").appendChild(li);
+  }
 
   //  --- 取得した言葉をタスク要素にする
   //  --- タスクリストに要素を追加する
